9e4c1e4a067c3a35b7a7718f780ed3875fd7df42
nualdaber <nurss.aldb@gmail.com>
2026-04-10 19:28:48 +0500

    feat: added main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import interfaces.MyList;
+import models.MyArrayList;
+import models.MyLinkedList;
+import structures.MyStack;
+import structures.MyQueue;
+import structures.MyMinHeap;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        testList(new MyArrayList<Integer>());
+        testList(new MyLinkedList<Integer>());
+        testStack();
+        testQueue();
+        testHeap();
+    }
+
+    private static void testList(MyList<Integer> list) {
+        list.add(10);
+        list.add(20);
+        list.addFirst(5);
+        list.add(1, 15);
+        list.sort();
+
+        for (Integer i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println("| Size: " + list.size());
+
+        list.remove(0);
+        list.clear();
+    }
+
+    private static void testStack() {
+        MyStack<String> stack = new MyStack<>();
+        stack.push("A");
+        stack.push("B");
+        System.out.println("Stack Pop: " + stack.pop());
+    }
+
+    private static void testQueue() {
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        System.out.println("Queue Dequeue: " + queue.dequeue());
+    }
+
+    private static void testHeap() {
+        MyMinHeap<Integer> heap = new MyMinHeap<>();
+        heap.insert(50);
+        heap.insert(10);
+        heap.insert(30);
+        heap.insert(5);
+        System.out.println("Min Heap Extract: " + heap.extractMin());
     }
 }
